e304e0903083762803b3e9b4fa1bb043520a4f1d
Nathan <satchmoturner@gmail.com>
2023-03-15 09:49:20 -0400

    new micropost

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
-
   def hello
-      render html: "hello world!"
+      render html: "hello world! how are you doing? I have wondered if everything is ok with you and everybody over there so if you know definietley tel ll me ok??????? thanks you i love you have a good days!"
+  end
+
 end
